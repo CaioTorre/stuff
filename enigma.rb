56863c5698ee29cbce7d5315597d5e568ddbbc1c
Caio Torre <caiosanches@live.com>
2015-05-04 16:24:30 -0300

    Added a bunch of new functions
    just read lines from 227 to 240

--- enigma.rb
+++ enigma.rb
@@ -23,6 +23,7 @@ puts
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 #alphabetH = Hash.(alphabet.map.with_index.to_a)
 alphabetH = { "a" => 0, "b" => 1, "c" => 2, "d" => 3, "e" => 4, "f" => 5, "g" => 6, "h" => 7, "i" => 8, "j" => 9, "k" => 10, "l" => 11, "m" => 12, "n" => 13, "o" => 14, "p" => 15, "q" => 16, "r" => 17, "s" => 18, "t" => 19, "u" => 20, "v" => 21, "w" => 22, "x" => 23, "y" => 24, "z" => 25 }
+savedOutputs = []
 while true
     maintain = true
     result = ""
@@ -32,22 +33,22 @@ while true
     thirdMod = 0
     keys = [16, 4, 21, 9, 25]
     #define Rotors
-    rotorOneBR =     [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
-    rotorTwoBR =     [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]
-    rotorThreeBR =    [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
-    rotorFourBR =     [4, 18, 14, 21, 15, 25, 9, 0, 24, 16, 20, 8, 17, 7, 23, 11, 13, 5, 19, 6, 10, 3, 2, 12, 22, 1]
-    rotorFiveBR =     [21, 25, 1, 17, 6, 8, 19, 24, 20, 15, 18, 3, 13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 10]
-    rotorOneAR =     [20, 22, 24, 6, 0, 3, 5, 15, 21, 25, 1, 4, 2, 10, 12, 19, 7, 23, 18, 11, 17, 8, 13, 16, 14, 9]
-    rotorTwoAR =     [0, 9, 15, 2, 25, 22, 17, 11, 5, 1, 3, 10, 14, 19, 24, 20, 16, 6, 4, 13, 7, 23, 12, 8, 21, 18]
-    rotorThreeAR =     [19, 0, 6, 1, 15, 2, 18, 3, 16, 4, 20, 5, 21, 13, 25, 7, 24, 8, 23, 9, 22, 11, 17, 10, 14, 12]
-    rotorFourAR =     [7, 25, 22, 21, 0, 17, 19, 13, 11, 6, 20, 15, 23, 16, 2, 4, 9, 12, 1, 18, 10, 3, 24, 14, 8, 5]
-    rotorFiveAR =     [16, 2, 24, 11, 23, 22, 4, 15, 5, 19, 25, 14, 18, 12, 21, 9, 20, 3, 10, 6, 8, 0, 17, 15, 7, 1]
-    allRotorsBR = [rotorOneBR, rotorTwoBR, rotorThreeBR, rotorFourBR, rotorFiveBR]
-    allRotorsAR = [rotorOneAR, rotorTwoAR, rotorThreeAR, rotorFourAR, rotorFiveAR]
+    rotorOneBR =     	[4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
+    rotorTwoBR =     	[0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]
+    rotorThreeBR =    	[1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
+    rotorFourBR =    	[4, 18, 14, 21, 15, 25, 9, 0, 24, 16, 20, 8, 17, 7, 23, 11, 13, 5, 19, 6, 10, 3, 2, 12, 22, 1]
+    rotorFiveBR =    	[21, 25, 1, 17, 6, 8, 19, 24, 20, 15, 18, 3, 13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 10]
+    rotorOneAR =     	[20, 22, 24, 6, 0, 3, 5, 15, 21, 25, 1, 4, 2, 10, 12, 19, 7, 23, 18, 11, 17, 8, 13, 16, 14, 9]
+    rotorTwoAR =     	[0, 9, 15, 2, 25, 22, 17, 11, 5, 1, 3, 10, 14, 19, 24, 20, 16, 6, 4, 13, 7, 23, 12, 8, 21, 18]
+    rotorThreeAR =     	[19, 0, 6, 1, 15, 2, 18, 3, 16, 4, 20, 5, 21, 13, 25, 7, 24, 8, 23, 9, 22, 11, 17, 10, 14, 12]
+    rotorFourAR =     	[7, 25, 22, 21, 0, 17, 19, 13, 11, 6, 20, 15, 23, 16, 2, 4, 9, 12, 1, 18, 10, 3, 24, 14, 8, 5]
+    rotorFiveAR =     	[16, 2, 24, 11, 23, 22, 4, 15, 5, 19, 25, 14, 18, 12, 21, 9, 20, 3, 10, 6, 8, 0, 17, 15, 7, 1]
+    allRotorsBR = 		[rotorOneBR, rotorTwoBR, rotorThreeBR, rotorFourBR, rotorFiveBR]
+    allRotorsAR = 		[rotorOneAR, rotorTwoAR, rotorThreeAR, rotorFourAR, rotorFiveAR]
     #Define Reflectors
-    reflectorB =     [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
-    reflectorC =     [5, 21, 15, 9, 8, 0, 14, 24, 4, 3, 17, 25, 23, 22, 6, 2, 19, 10, 20, 16, 18, 1, 13, 12, 7, 11]
-    allReflectors = [reflectorB, reflectorC]
+    reflectorB =     	[24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
+    reflectorC =     	[5, 21, 15, 9, 8, 0, 14, 24, 4, 3, 17, 25, 23, 22, 6, 2, 19, 10, 20, 16, 18, 1, 13, 12, 7, 11]
+    allReflectors = 	[reflectorB, reflectorC]
     isOk = false
     while !isOk
         print "First rotor id: "
@@ -201,11 +202,13 @@ while true
     currRotTwoAR = allRotorsAR[sRID]
     currRotThreeBR = allRotorsBR[tRID]
     currRotThreeAR = allRotorsAR[tRID]
+	writtenOutputs = []
     while maintain
         #print "Input: "
         inputCMD = waitForSafeInput(false, "Input: ")
         case inputCMD
         when "over"
+		writtenOutputs = []
             puts
             puts "+++++++++++RESTARTING+++++++++++"
             puts
@@ -221,6 +224,20 @@ while true
 		when "clear"
 			printSpacer = ""
 			result = ""
+		when "write"
+			writtenOutputs.insert(0, result)
+		when "save"
+			savedOutputs.insert(0, result)
+		when "output written"
+			print writtenOutputs
+			puts
+		when "output saved"
+			print savedOutputs
+			puts
+		when "clear saved"
+			savedOutputs = []
+		when "clear written"
+			writtenOutputs = []
         else
 			#if (!inputCMD.empty?)
             inputLetter = inputCMD
